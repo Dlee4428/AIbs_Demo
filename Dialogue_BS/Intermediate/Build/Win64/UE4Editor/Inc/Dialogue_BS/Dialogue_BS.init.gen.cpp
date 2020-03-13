4ef517546b0b4c2514ac9ed267bbba28e9550eda
Gunjudge <45321606+Gunjudge@users.noreply.github.com>
2020-03-12 21:04:43 -0400

    Made displaying info smaller/more efficient
    Changed NPCand NPC_AI. NPC_AI has method for getting and displaying values. NPC got more value text as well as a new array variable to make dsplay easier.

--- Dialogue_BS/Intermediate/Build/Win64/UE4Editor/Inc/Dialogue_BS/Dialogue_BS.init.gen.cpp
+++ Dialogue_BS/Intermediate/Build/Win64/UE4Editor/Inc/Dialogue_BS/Dialogue_BS.init.gen.cpp
@@ -19,8 +19,8 @@ void EmptyLinkFunctionForGeneratedCodeDialogue_BS_init() {}
 			static const UE4CodeGen_Private::FPackageParams PackageParams = {
 				"/Script/Dialogue_BS",
 				PKG_CompiledIn | 0x00000000,
-				0x11401859,
-				0x949E4AA9,
+				0x2CF32EBD,
+				0xB3905C65,
 				nullptr, 0,
 				METADATA_PARAMS(nullptr, 0)
 			};
